7f8d7c7bdab488dc7d266231bcdf9e5b6156d184
Kaushik Gopal <code@kaush.co>
2013-03-30 11:49:14 -0700

    stuff again

--- spec/gol_spec.rb
+++ spec/gol_spec.rb
@@ -2,15 +2,24 @@ require 'spec_helper'
 
 describe GameOfLife do
 
+	let(:gol) { GameOfLife.new(100, 120) }
+
 	context "Board" do
 		it "should have a boundary" do
-			gol = GameOfLife.new(100, 100)
-			expect(gol.boundary).to be_a(Array)
+			expect(gol.boundary[0]).to eql(100)
+			expect(gol.boundary[1]).to eql(120)
 		end
 	end
 
 	context "Cell" do
-		it "should have a state"
+		let(:cell) { Cell.new }
+		it "should have a default state of dead" do
+			expect(cell.alive?).to eql(false)
+		end
+		it "should have an option to become alive" do
+			cell.alive = true
+			expect(cell.alive?).to eql(false)
+		end
 		it "should have a location"
 	end
 
